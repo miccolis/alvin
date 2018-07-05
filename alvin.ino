61c07e108f455264d8960c66290631925008ca5e
Jeff Miccolis <jeff@miccolis.net>
2018-07-05 11:35:30 -0400

    Display Uid to LCD

--- alvin.ino
+++ alvin.ino
@@ -9,6 +9,8 @@ Adafruit_PCD8544 display = Adafruit_PCD8544(18, 20, 19);
 
 const int blPin = 10;
 
+MFRC522::Uid activeUid;
+
 void setup() {
 
   pinMode(blPin, OUTPUT);
@@ -20,12 +22,8 @@ void setup() {
   display.setContrast(60);
 
   display.display(); // show splashscreen
-  delay(2000);
+  delay(1000);
   display.clearDisplay();
-
-  Serial.begin(9600);
-  mfrc522.PCD_Init();
-  mfrc522.PCD_DumpVersionToSerial();
 
   display.setTextSize(1);
   display.setTextColor(BLACK);
@@ -33,6 +31,10 @@ void setup() {
   display.println("Ready!");
   display.display();
   delay(2000);
+
+  Serial.begin(9600);
+  mfrc522.PCD_Init();
+  mfrc522.PCD_DumpVersionToSerial();
 }
 
 void loop() {
@@ -46,6 +48,39 @@ void loop() {
             return;
     }
 
+    copyUid(&(mfrc522.uid), &activeUid);
+
     // Dump debug info about the card; PICC_HaltA() is automatically called
     mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+
+    delay(100);
+    display.clearDisplay();
+    display.println("Card read complete!");
+    printUid(&activeUid);
+    display.display();
+    delay(2000);
+    display.clearDisplay();
+    display.println("Ready!");
+    display.display();
+
+}
+
+void copyUid(MFRC522::Uid* src, MFRC522::Uid* dest) {
+    dest->size = src->size;
+    dest->sak = src->sak;
+    for(int i = 0; i < src->size; i++) {
+        dest->uidByte[i] = src->uidByte[i];
+    }
+}
+
+void printUid(MFRC522::Uid* uid) {
+    display.print(F("Card UID:"));
+    for (byte i = 0; i < uid->size; i++) {
+        if(uid->uidByte[i] < 0x10)
+            display.print(F(" 0"));
+        else
+            display.print(F(" "));
+        display.print(uid->uidByte[i], HEX);
+    }
+    display.println();
 }
